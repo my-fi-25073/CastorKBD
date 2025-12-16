e68bd6ef1d334145061622888bbf06b0a6b85366
my-fi <myfi25073@gmail.com>
2025-12-16 18:36:46 +0900

    Enhance Castor keyboard configuration: removed USB suspend wakeup delay, added USART PAL mode settings, and enabled split watchdog for improved performance. Updated rules.mk to support serial transport and USART I/O.

--- FirmwareFiles/castor_kbd/config.h
+++ FirmwareFiles/castor_kbd/config.h
@@ -2,7 +2,6 @@
 
 // Handedness by Pin - PA15
 #define SPLIT_HAND_PIN A15
-#define USB_SUSPEND_WAKEUP_DELAY 200
 #define DYNAMIC_KEYMAP_LAYER_COUNT 6
 
 // Split USART Full-duplex 설정
@@ -10,3 +9,8 @@
 #define SERIAL_USART_TX_PIN A2
 #define SERIAL_USART_RX_PIN A3
 #define SERIAL_USART_DRIVER SD2  // PA2/PA3는 USART2
+#define SERIAL_USART_TX_PAL_MODE 1
+#define SERIAL_USART_RX_PAL_MODE 1
+
+// Split watchdog for slave timeout issues
+#define SPLIT_WATCHDOG_ENABLE
